f4bbd25739706df1ef79c9ca4c8e732b5ba8e183
Erick Marin <erick.marin.v@gmail.com>
2025-10-28 17:54:14 -0600

    Avances

--- duolingo/assets/js/controlador.js
+++ duolingo/assets/js/controlador.js
@@ -1,2 +1,129 @@
 console.log("Categorias: ", categorias);
 console.log("Usuarios: ", usuarios);
+var indicePreguntaActual = 0;
+var idCategoriaActual = null;
+var respuestasCorrectas = 0;
+var preguntaContestada = false;
+
+
+const mostrarSeccionUsuarios = () => { //Arrow function
+    console.log("Mostrar seccion usuarios");
+    document.getElementById("seccion-categorias").style.display = "none";
+    document.getElementById("seccion-preguntas").style.display = "none";
+    document.getElementById("seccion-usuarios").style.display = "block";
+}
+
+const mostrarSeccionCategorias = () => {
+    console.log("Mostrar seccion categorias");
+    preguntaContestada = false;
+    respuestasCorrectas = 0;
+
+    document.getElementById("seccion-usuarios").style.display = "none";
+    document.getElementById("seccion-preguntas").style.display = "none";
+    document.getElementById("seccion-categorias").style.display = "block";
+}
+
+const mostrarSeccionPreguntas = () => {
+    console.log("Mostrar seccion preguntas");
+    document.getElementById("seccion-usuarios").style.display = "none";
+    document.getElementById("seccion-categorias").style.display = "none";
+    document.getElementById("seccion-preguntas").style.display = "block";
+}
+
+const renderizarListaUsuarios = () => {
+    document.getElementById("contenedor-usuarios").innerHTML = "";
+    for (let i=0; i< usuarios.length; i++) {
+        document.getElementById("contenedor-usuarios").innerHTML += 
+                    `<div>
+                        <div class="opcion-usuario" onclick="renderizarUsuarioSeleccionado(${usuarios[i].id}); mostrarSeccionCategorias()">
+                            <img src="assets/img/${usuarios[i].imagenPerfil}">
+                            <div>${usuarios[i].nombre}</div>
+                        </div>
+                    </div>`;
+    }
+}
+
+const renderizarListaCategorias = () => {
+    respuestasCorrectas = 0;
+    categorias.forEach(categoria => {
+        document.getElementById("contenedor-categorias").innerHTML += 
+                `<div onclick="renderizarPregunta(${categoria.id}, 0); mostrarSeccionPreguntas();">
+                    <div id="categoria-${categoria.id}" class="icono-categoria" style="background-color: ${categoria.color}">
+                        <i class="${categoria.icono}"></i>
+                    </div>
+                    <div>
+                        ${categoria.nombre}
+                    </div>
+                </div>`;
+    });    
+}
+
+const renderizarPregunta = (idCategoria, indicePregunta) => {
+    idCategoriaActual = idCategoria;
+    const categoriaSeleccionada = categorias.find(categoria => categoria.id === idCategoria);
+    console.log("Categoria seleccionada: ", categoriaSeleccionada);
+
+    if (indicePregunta >= categoriaSeleccionada.preguntas.length) {
+        alert("¡Has completado la categoría!");
+        indicePreguntaActual = 0;
+        mostrarSeccionCategorias();
+        return;
+    }
+    const preguntaSeleccionada = categoriaSeleccionada.preguntas[indicePregunta]; //Por ahora solo la primera pregunta
+    document.getElementById("palabra").innerHTML = preguntaSeleccionada.palabra;
+
+    //Renderizar las opciones de respuestas
+    document.getElementById("opciones-respuestas").innerHTML = "";
+    preguntaSeleccionada.respuestas.forEach(respuesta => {
+        document.getElementById("opciones-respuestas").innerHTML += 
+            `<div>
+                <div class="opcion-respuesta" onclick="verificarRespuesta('${respuesta.palabra}', ${respuesta.correcta}, this)">${respuesta.palabra}</div>
+            </div>`;
+    });
+
+    document.getElementById("numero-pregunta-actual").innerText = indicePregunta + 1;
+    document.getElementById("numero-pregunta-total").innerText = categoriaSeleccionada.preguntas.length;
+}
+
+const renderizarUsuarioSeleccionado = (idUsuario) => {
+    const usuarioSeleccionado = usuarios.find(usuario => usuario.id === idUsuario);
+    console.log("Usuario seleccionado: ", usuarioSeleccionado);
+    categorias.forEach(categoria => {
+        document.getElementById('categoria-'+categoria.id).classList.remove('aprobada');
+    });
+
+
+    // Marcar las categorias aprobadas
+    usuarioSeleccionado.resultados.forEach(resultado => {
+        if (resultado.aprobada){
+            document.getElementById('categoria-'+resultado.category).classList.add('aprobada');
+        }
+    });
+
+
+    document.getElementById("img-usuario").attributes.src.value = `assets/img/${usuarioSeleccionado.imagenPerfil}`;
+    document.getElementById("contador-coronas").innerText = usuarioSeleccionado.coronas;
+    document.getElementById("contador-vidas").innerText = usuarioSeleccionado.vidas;
+}
+
+const renderizarSiguientePregunta = () => {
+    preguntaContestada = false;
+    indicePreguntaActual++;
+    renderizarPregunta(idCategoriaActual, indicePreguntaActual);
+}
+
+const verificarRespuesta = (palabra, correcta, etiqueta) => {
+    if (preguntaContestada) 
+        return;
+    console.log("Respuesta seleccionada: ", palabra, "Correcta: ", correcta);
+    if (correcta) {
+        etiqueta.classList.add("correcta");
+        respuestasCorrectas++;
+    } else {
+        etiqueta.classList.add("incorrecta");
+    }
+    preguntaContestada = true;
+    console.log("Respuestas correctas hasta ahora: ", respuestasCorrectas); 
+}
+renderizarListaCategorias();
+renderizarListaUsuarios();
--- duolingo/assets/js/data.js
+++ duolingo/assets/js/data.js
@@ -1,8 +1,516 @@
 var categorias = [
-    {
+  {
+    id: 1,
+    nombre: "Family",
+    icono: "fa-solid fa-people-roof",
+    color: "#F27B7B",
+    preguntas: [
+      {
         id: 1,
-        nombre: "Family",
-        icono: "fa-solid fa-people-roof"
-    }
+        palabra: "Hermana",
+        respuestas: [
+          {
+            palabra: "Brother",
+            correcta: false,
+          },
+          {
+            palabra: "Mother",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Sister",
+            correcta: true,
+          },
+        ],
+      },
+      {
+        id: 2,
+        palabra: "Familia",
+        respuestas: [
+          {
+            palabra: "Family",
+            correcta: true,
+          },
+          {
+            palabra: "Mother",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 3,
+        palabra: "Padre",
+        respuestas: [
+          {
+            palabra: "Father",
+            correcta: true,
+          },
+          {
+            palabra: "Mother",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 4,
+        palabra: "Hijo",
+        respuestas: [
+          {
+            palabra: "Son",
+            correcta: true,
+          },
+          {
+            palabra: "Daughter",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 5,
+        palabra: "Hija",
+        respuestas: [
+          {
+            palabra: "Daughter",
+            correcta: true,
+          },
+          {
+            palabra: "Son",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    nombre: "Animals",
+    icono: "fa-solid fa-paw",
+    color: "#F2A900",
+    preguntas: [
+      {
+        id: 1,
+        palabra: "Perro",
+        respuestas: [
+          {
+            palabra: "Dog",
+            correcta: true,
+          },
+          {
+            palabra: "Cat",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 2,
+        palabra: "Gato",
+        respuestas: [
+          {
+            palabra: "Cat",
+            correcta: true,
+          },
+          {
+            palabra: "Dog",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 3,
+        palabra: "Pájaro",
+        respuestas: [
+          {
+            palabra: "Bird",
+            correcta: true,
+          },
+          {
+            palabra: "Dog",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 4,
+        palabra: "Pez",
+        respuestas: [
+          {
+            palabra: "Fish",
+            correcta: true,
+          },
+          {
+            palabra: "Dog",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 5,
+        palabra: "Caballo",
+        respuestas: [
+          {
+            palabra: "Horse",
+            correcta: true,
+          },
+          {
+            palabra: "Dog",
+            correcta: false,
+          },
+          {
+            palabra: "Water",
+            correcta: false,
+          },
+          {
+            palabra: "Java",
+            correcta: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    nombre: "Events",
+    icono: "fa-solid fa-calendar-days",
+    color: "#56C2A1",
+    preguntas: [
+      {
+        id: 1,
+        palabra: "Cumpleaños",
+        respuestas: [
+          {
+            palabra: "Birthday",
+            correcta: true,
+          },
+          {
+            palabra: "Aniversario",
+            correcta: false,
+          },
+          {
+            palabra: "Fiesta",
+            correcta: false,
+          },
+          {
+            palabra: "Regalo",
+            correcta: false,
+          },
+        ],
+      },
+      {
+        id: 2,
+        palabra: "Navidad",
+        respuestas: [
+          {
+            palabra: "Christmas",
+            correcta: true,
+          },
+          {
+            palabra: "Easter",
+            correcta: false,
+          },
+          {
+            palabra: "Halloween",
+            correcta: false,
+          },
+          {
+            palabra: "Thanksgiving",
+            correcta: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    nombre: "Food",
+    icono: "fa-solid fa-burger",
+    color: "#91C46C",
+    preguntas: [
+      {
+        id: 1,
+        palabra: "Manzana",
+        respuestas: [
+          {
+            palabra: "Apple",
+            correcta: true,
+          },
+          {
+            palabra: "Banana",
+            correcta: false,
+          },
+          {
+            palabra: "Orange",
+            correcta: false,
+          },
+          {
+            palabra: "Grapes",
+            correcta: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    nombre: "Colors",
+    icono: "fa-solid fa-palette",
+    color: "#D16A9A",
+    preguntas: [
+      {
+        id: 1,
+        palabra: "Rojo",
+        respuestas: [
+          {
+            palabra: "Red",
+            correcta: true,
+          },
+          {
+            palabra: "Blue",
+            correcta: false,
+          },
+          {
+            palabra: "Green",
+            correcta: false,
+          },
+          {
+            palabra: "Yellow",
+            correcta: false,
+          },
+        ],
+      },
+    ],
+  }
 ];
-var usuarios = [];
+
+var usuarios = [
+  {
+    id: 1,
+    nombre: "Goku",
+    imagenPerfil: "profile-pics/goku.jpg",
+    coronas: 2,
+    vidas: 5,
+    resultados: [
+      {
+        category: 1,
+        correctas: 4,
+        incorrectas: 1,
+        aprobada: false,
+      },
+      {
+        category: 2,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 3,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      // ...
+    ],
+  },
+  {
+    id: 2,
+    nombre: "Vegeta",
+    imagenPerfil: "profile-pics/vegeta.jpg",
+    coronas: 3,
+    vidas: 4,
+    resultados: [
+        {
+        category: 1,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 2,
+        correctas: 4,
+        incorrectas: 1,
+        aprobada: false,
+      },
+      {
+        category: 3,
+        correctas: 4,
+        incorrectas: 1,
+        aprobada: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    nombre: "Krillin",
+    imagenPerfil: "profile-pics/krilin.jpg",
+    coronas: 3,
+    vidas: 4,
+    resultados: [
+      {
+        category: 1,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 2,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 3,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    nombre: "Nappa",
+    imagenPerfil: "profile-pics/nappa.jpg",
+    coronas: 3,
+    vidas: 4,
+    resultados: [
+      {
+        category: 1,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 2,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 3,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    nombre: "Freezer",
+    imagenPerfil: "profile-pics/freezer.jpg",
+    coronas: 3,
+    vidas: 4,
+    resultados: [
+      {
+        category: 1,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 2,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 3,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    nombre: "Dende",
+    imagenPerfil: "profile-pics/dende.jpg",
+    coronas: 3,
+    vidas: 4,
+    resultados: [
+      {
+        category: 1,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 2,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+      {
+        category: 3,
+        correctas: 5,
+        incorrectas: 0,
+        aprobada: true,
+      },
+    ],
+  },
+];
